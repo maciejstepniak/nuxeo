maciejstepniak/nuxeo

commit d127208d44caf8fc9e20b45b9c3ef96ce39e2585

NXP-28995: improve all sorts of remaining doc + add tests

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/resources/skin/views/contribution/default.ftl
@@ -13,8 +13,12 @@
   This contribution is part of XML component <a class="tag components" href="${Root.path}/${distId}/viewComponent/${nxItem.component.id}">${nxItem.component.id}</a>
   inside ${nxItem.component.bundle.fileName} ${nxItem.component.xmlFileName}
 
-  <h2>Documentation</h2>
-  ${nxItem.documentationHtml}
+  <#if nxItem.documentationHtml?has_content>
+    <h2>Documentation</h2>
+    <div class="documentation">
+      ${nxItem.documentationHtml}
+    </div>
+  </#if>
 
   <h2>Extension point</h2>
   Extension point
@@ -33,8 +37,12 @@
         <div class="block-title">
           <input id="${contributionItem.id}" type="checkbox" name="${contributionItem.id}" value="${contributionItem.id}" style="display:none" checked/>
           <label for="${contributionItem.id}">${contributionItem.label}</label>
-          <span>${contributionItem.documentation}</span>
         </div>
+        <#if nxItem.documentationHtml?has_content>
+          <div class="block-description">
+            ${contributionItem.documentation}
+          </div>
+        </#if>
       </li>
     </#list>
     </ul>

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/resources/skin/views/component/default.ftl
@@ -10,8 +10,12 @@
 
 <div class="tabscontent">
 
-  <h2>Documentation</h2>
-  ${nxItem.documentationHtml}
+  <#if nxItem.documentationHtml?has_content>
+    <h2>Documentation</h2>
+    <div class="documentation">
+      ${nxItem.documentationHtml}
+    </div>
+  </#if>
 
   <#if nxItem.requirements?size gt 0>
     <h2>Requirements</h2>

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/resources/skin/views/extensionPoint/default.ftl
@@ -16,9 +16,13 @@
       <li><a href="#contribute">Existing Contributions</a></li>
     </ul>
   </div>
-  <div class="description">
-    ${nxItem.documentationHtml}
-  </div>
+
+  <#if nxItem.documentationHtml?has_content>
+    <h2>Documentation</h2>
+    <div class="documentation">
+      ${nxItem.documentationHtml}
+    </div>
+  </#if>
 
   <h2>Contribution Descriptor</h2>
   <ul>
@@ -29,7 +33,7 @@
   </ul>
 
   <#if nxItem.extensions?size gt 0>
-    <h2 id="contribute">Existing Contributions </h2>
+    <h2 id="contribute">Existing Contributions</h2>
     <input type="search" id="searchField" placeholder="Text in contributions"/>
     <input type="button" value="search" onclick="searchContrib($('#searchField').val());"/>
     <span id="searchMatchResult"></span>
@@ -74,7 +78,7 @@
           ${contrib.component.bundle.fileName} ${contrib.component.xmlFileName}
           </a>
           &nbsp;
-          <a class="override" href="${Root.path}/${distId}/viewComponent/${contrib.component.id}/override/?contributionId=${contrib.id}" target="_blank">
+          <a class="override button" href="${Root.path}/${distId}/viewComponent/${contrib.component.id}/override/?contributionId=${contrib.id}" target="_blank">
           Override
           </a>
         </div>

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/resources/skin/views/service/default.ftl
@@ -10,9 +10,6 @@
 
 <div class="tabscontent">
 
-  <h2>Documentation</h2>
-  ${nxItem.documentationHtml}
-
   <h2>Implementation</h2>
   <p><b>${nxItem.id}</b></p>
   <p><div id="shortjavadoc" class="description"></div></p>

ftests/nuxeo-platform-explorer-ftests/src/test/java/org/nuxeo/functionaltests/explorer/AbstractExplorerTest.java
@@ -155,9 +155,10 @@ public abstract class AbstractExplorerTest extends AbstractTest {
         listing.checkListing(-1, "actions.ActionService", "/viewComponent/org.nuxeo.ecm.platform.actions.ActionService",
                 "JAVA org.nuxeo.ecm.platform.actions.ActionService");
 
-        listing = listing.filterOn("org.nuxeo.apidoc");
-        listing.checkListing(6, "apidoc.adapterContrib", "/viewComponent/org.nuxeo.apidoc.adapterContrib",
-                "XML org.nuxeo.apidoc.adapterContrib");
+        listing = listing.filterOn("org.nuxeo.apidoc").toggleSort();
+        listing.checkListing(6, "apidoc.snapshot.SnapshotManagerComponent",
+                "/viewComponent/org.nuxeo.apidoc.snapshot.SnapshotManagerComponent",
+                "JAVA org.nuxeo.apidoc.snapshot.SnapshotManagerComponent");
 
         listing.navigateToFirstItem();
         ComponentArtifactPage apage = asPage(ComponentArtifactPage.class);

ftests/nuxeo-platform-explorer-ftests/src/test/java/org/nuxeo/functionaltests/explorer/ITExplorerTest.java
@@ -136,7 +136,7 @@ public class ITExplorerTest extends AbstractExplorerTest {
 
     @Test
     public void testContributionsAlternative() {
-        goToArtifact(ExtensionInfo.TYPE_NAME, "org.nuxeo.apidoc.doctypeContrib--doctype");
+        goToArtifact(ExtensionInfo.TYPE_NAME, "org.nuxeo.apidoc.listener.contrib--listener");
         ContributionArtifactPage apage = asPage(ContributionArtifactPage.class);
         apage.checkAlternative();
     }

ftests/nuxeo-platform-explorer-ftests/src/test/java/org/nuxeo/functionaltests/explorer/pages/artifacts/ArtifactPage.java
@@ -76,12 +76,7 @@ public abstract class ArtifactPage extends AbstractExplorerPage {
     }
 
     public void checkDescription(String expected) {
-        try {
-            assertEquals(expected, description.getText());
-        } catch (NoSuchElementException e) {
-            // description is not mandatory on all pages
-            assertNull(expected);
-        }
+        checkTextIfExists(expected, description);
     }
 
     public void checkDocumentationText(String expected) {

ftests/nuxeo-platform-explorer-ftests/src/test/java/org/nuxeo/functionaltests/explorer/pages/artifacts/ComponentArtifactPage.java
@@ -34,9 +34,12 @@ public class ComponentArtifactPage extends ArtifactPage {
 
     @Override
     public void checkReference() {
-        checkCommon("Component org.nuxeo.apidoc.adapterContrib", "Component org.nuxeo.apidoc.adapterContrib",
-                "In bundle org.nuxeo.apidoc.repo");
+        checkCommon("Component org.nuxeo.apidoc.snapshot.SnapshotManagerComponent",
+                "Component org.nuxeo.apidoc.snapshot.SnapshotManagerComponent", "In bundle org.nuxeo.apidoc.repo");
         checkRequirements(null);
+        checkDocumentationText(
+                "This component handles the introspection of the current live Runtime as a distribution.\n" //
+                        + "It can also persist this introspection as Nuxeo documents, to handle import and export of external distributions.");
     }
 
     @Override
@@ -44,6 +47,7 @@ public class ComponentArtifactPage extends ArtifactPage {
         checkCommon("Component org.nuxeo.ecm.automation.server.marshallers",
                 "Component org.nuxeo.ecm.automation.server.marshallers", "In bundle org.nuxeo.ecm.automation.io");
         checkRequirements(List.of("org.nuxeo.ecm.platform.contentview.json.marshallers"));
+        checkDocumentationText(null);
     }
 
     @Override

commit cac0f97a105d6ad202427519ff9c8c9fb3296d5d

NXP-28995: fix documentation display on contributions
Bug introduced by commit d127208d44caf8fc9e20b45b9c3ef96ce39e2585.
Was not visible in ftests where the parent contribution holds documentation too...

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/resources/skin/views/contribution/default.ftl
@@ -39,7 +39,7 @@
           <input id="${contributionItem.id}" type="checkbox" name="${contributionItem.id}" value="${contributionItem.id}" style="display:none" checked/>
           <label for="${contributionItem.id}">${contributionItem.label}</label>
         </div>
-        <#if nxItem.documentationHtml?has_content>
+        <#if contributionItem.documentation?has_content>
           <div class="block-description">
             ${contributionItem.documentation}
           </div>